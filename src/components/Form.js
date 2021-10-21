3c95a24077a4fade0cdee0449e1316eafbdd4621
Tahmina <tahmina.choudhury@lc.cuny.edu>
2021-10-21 12:31:52 -0400

    favLLinks

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,31 +8,52 @@ class Form extends Component {
             TODO - set initial state for link name and URL 
 
         */
+
+            this.state = {name: " ", URL: " "};
+
+            this.handleChange = this.handleChange.bind(this);
+            this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
     handleChange = event => {
         /*
             TODO - Logic for changing state based on form changes
         */
+
+            if (event.target.id  === "name") {
+                this.setState({name: event.target.value });
+
+            } else{
+                this.setState({URL: event.target.value});
+            }
     }
 
-    onFormSubmit = (event) => {
+   handleSubmit = (event) => {
         // to prevent page reload on form submit
+
+       let newLink = {name: this.state.name, URL: this.state.URL}
+       this.props.newSubmit(newLink)
         event.preventDefault();
         
         /*
             TODO - Logic for calling props to handle submission and setting state changes
         */
 
+
+
+
     }
 
     render() {
 
         return(
-            <form>
+            <form> onFormSubmit={this.handleSubmit}
                 {/* TODO - Logic for returning a form element with labels and inputs for link name and URL */}
+                
+                <><label>Name</label><input type="text" onInput={(event) => this.handleChange(event)} /><label>URL</label><input type="text" onInput={(event) => this.handleChange(event)} /><button type="submitForm" onClick={this.onFormSubmit} /></>
+                
             </form>
-        )
+        );
     
     }
 }
--- src/components/LinkContainer.js
+++ src/components/LinkContainer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Table from './Table';
 // import Table from './Table';
 // import Form from './Form';
 
@@ -6,18 +7,26 @@ class LinkContainer extends React.Component {
   constructor(props) {
     super(props)
     /* TODO - Create state object for storing favLinks */
+
+    this.state = { favLinks: []}
   }
 
   handleRemove = (index) => {
     /*
             TODO - Create logic for setting the state to filter array and remove favLink at index
         */
+
+            let favLink = this.state.favLinks.filter((ele, ind) => ind !== index);
+            this.setState({favLinks:  favLink})
   }
 
   handleSubmit = (favLink) => {
     /*
             TODO - Create logic to setState and add new favLink to favLinks array in state
         */
+
+            this.setState({favLinks: [...this.state.favLinks, favLink]})
+
   }
 
   render() {
@@ -26,11 +35,14 @@ class LinkContainer extends React.Component {
         <h1>My Favorite Links</h1>
         <p>Add a new url with a name and link to the table.</p>
         {/*TODO - Add Table Component */}
+        <Table linkData = {this.state.favLinks} removeLink = {this.handleRemove} />
 
         <br />
 
         <h3>Add New</h3>
         {/*TODO - Add Form Component */}
+        <Form newSubmit = {this.handleSubmit} />
+        
       </div>
     )
   }
